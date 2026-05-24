395611679e5bac9f1d609cf425b43be00ea89f0f
aayushman jangra <jan291741@gmail.com>
2026-05-24 08:54:48 +0530

    nothing special

--- main.cpp
+++ main.cpp
@@ -1,9 +1,30 @@
 #include <iostream>
 #include "vm.h"
 #include <vector>
+#include <fstream>
 
-int main()
+using std::string , std::ifstream , std::getline, std::vector, std::cout;
+
+
+int main(int argc, char* argv[])
 {
-    
+    string btcode = "";
+
+    ifstream file(argv[1]);
+    string line = "";
+
+    if (file.is_open()) {
+        while (getline(file, line)) {
+            btcode += line + "\n";
+        }
+        file.close();
+    }
+    else
+    {
+        cout << "Unable to open file: " << argv[1] << "\n";
+        return 1;
+    }
+
+
     return 0;    
 }
--- vm.h
+++ vm.h
@@ -4,10 +4,43 @@
 
 struct Register
 {
-    
+    char charValue;
+    double doubleValue;
+    int intValue;
+
+    Register(){}
+
+    void setCharValue(char value) 
+    {
+        charValue = value; 
+    }
+    void setDoubleValue(double value) 
+    {
+        doubleValue = value; 
+    }
+    void setIntValue(int value) 
+    {
+        intValue = value; 
+    }
 };
 
 struct VM
 {
+    std::vector<Register> registers; 
 
-};
+    void createRegister()
+    {
+        Register reg;
+
+        reg.charValue = ' ';
+        reg.doubleValue = 0.0;
+        reg.intValue = 0;
+
+        registers.push_back(reg);
+    }
+
+    void main()
+    {
+
+    }
+};  
